484072abe3e070738c0bc3a859378d92f9505644
Sam <sam.frieten@hotmail.com>
2021-12-08 15:14:17 +0100

    All possible datasets
    1) Test cleaned -> No more missing values
    2) Train cleaned -> ALL outliers removed
    3) Train cleaned less restrictive -> most of the outliers are removed
    4) Train cleaned Na -> outliers were treated as NA values
    5) trained with outlier -> no outliers removed

--- src/data_cleaning.R
+++ src/data_cleaning.R
@@ -78,8 +78,8 @@ test_X_impute <- cbind(test_X_impute,
 #All data is lower than 25% missing so we can still accept this data and inpute values
 # NOW WE ARE GOING TO IMPUTE NUMERIC VARIABLES WITH MEAN AND CATEGORICAL VALUES WITH MODE
 
-#define the functions for imputing mean and mode            
-impute <- function(x, method = mean, val = NULL) {
+#define the functions for imputing median and mode            
+impute <- function(x, method = median, val = NULL) {
   if (is.null(val)) {
     val <- method(x, na.rm = TRUE)
   }
@@ -98,15 +98,15 @@ modus <- function(x, na.rm = FALSE) {
 num.cols <- sapply(train_X_impute, is.numeric) 
 train_X_impute[, num.cols] <- lapply(train_X_impute[, num.cols],
                                      FUN = impute,
-                                     method = mean)
+                                     method = median)
 
-test_X_impute$annual_income <- impute(test_X_impute$annual_income, val = mean(train_X$annual_income, na.rm = T))
-test_X_impute$num_bankrupts <- impute(test_X_impute$num_bankrupts, val = mean(train_X$num_bankrupts, na.rm = T))
-test_X_impute$num_records   <- impute(test_X_impute$num_records, val = mean(train_X$num_records, na.rm = T))
-test_X_impute$num_mortgages <- impute(test_X_impute$num_mortgages, val = mean(train_X$num_mortgages, na.rm = T))
-test_X_impute$num_total_credit <- impute(test_X_impute$num_total_credit, val = mean(train_X$num_total_credit, na.rm = T))
-test_X_impute$revol_util    <- impute(test_X_impute$revol_util, val = mean(train_X$revol_util, na.rm = T))
-test_X_impute$monthly_payment <- impute(test_X_impute$monthly_payment, val = mean(train_X$monthly_payment, na.rm = T))
+test_X_impute$annual_income <- impute(test_X_impute$annual_income, val = median(train_X$annual_income, na.rm = T))
+test_X_impute$num_bankrupts <- impute(test_X_impute$num_bankrupts, val = median(train_X$num_bankrupts, na.rm = T))
+test_X_impute$num_records   <- impute(test_X_impute$num_records, val = median(train_X$num_records, na.rm = T))
+test_X_impute$num_mortgages <- impute(test_X_impute$num_mortgages, val = median(train_X$num_mortgages, na.rm = T))
+test_X_impute$num_total_credit <- impute(test_X_impute$num_total_credit, val = median(train_X$num_total_credit, na.rm = T))
+test_X_impute$revol_util    <- impute(test_X_impute$revol_util, val = median(train_X$revol_util, na.rm = T))
+test_X_impute$monthly_payment <- impute(test_X_impute$monthly_payment, val = median(train_X$monthly_payment, na.rm = T))
 
 #determine the categoric columns and impute the mode in the train and test set
 cat.cols <- !num.cols
@@ -122,19 +122,17 @@ colMeans(is.na(train_X_impute))  # -->no more missing values
 colMeans(is.na(test_X_impute))   # -->no more missing values
 
 
+#For some non parametric models it is possible to work WITH outliers so we save one basetable
+
+
+write.csv(train_X_impute, file= "../data/silver/train_with_outlier.csv")
+write.csv(test_X_impute, file = "../data/silver/test_cleaned_data.csv")
+
 ###############################
-# 3. REMOVING OUTLIERS      ###   
+# 3.1 REMOVING OUTLIERS      ###   
 ###############################
 
 train_X_outlier = train_X_impute
-test_X_outlier = test_X_impute
-
-#let's define a function that removes outliers, but we don't really use it
-handle_outlier_z <- function(col){
-  col_z <- scale(col)
-  ifelse(abs(col_z)>3,
-         sign(col_z)*3*attr(col_z,"scaled:scale") + attr(col_z,"scaled:center"), col)
-}
 
 
 #below we can find a easy for loop that shows us which numeric variables contain outliers
@@ -160,27 +158,82 @@ b_ru <- abs(scale(train_X_outlier$revol_util)) < 3
 #we subset from our original dataset for which the outlier conditions are ALWAYS true,
 #in other words, if one of the conditions is false we drop the whole row
 new_train_x <- subset(train_X_outlier, (b_ai&b_dti&b_ir&b_mp&b_nb&b_nm&b_oc&b_nr&b_ntc&b_rb&b_ru))
-new_test_x <- test_X_outlier
 
 write.csv(new_train_x, file= "../data/silver/train_cleaned_data.csv")
-write.csv(new_test_x, file = "../data/silver/test_cleaned_data.csv")
 
 #we omitted 70954 -> 64943 = 6011 rows from our dataset
 
 
-###############################
-# 4. REMOVING SPECIFIC OUTLIERS 
-###############################
+################################
+# 3.2 Treat OUTLIERS as NA values 
+################################
 
-b_ai <- abs(scale(train_X_outlier$annual_income)) < 3
-b_dti <- abs(scale(train_X_outlier$debt_to_income)) < 3
-b_ir <- abs(scale(train_X_outlier$interest_rate)) < 3
-b_mp <- abs(scale(train_X_outlier$monthly_payment)) < 3
-b_nb <- abs(scale(train_X_outlier$num_bankrupts)) < 3
-b_nm <- abs(scale(train_X_outlier$num_mortgages)) < 3
-b_oc <- abs(scale(train_X_outlier$num_open_credit)) < 3
-b_nr <- abs(scale(train_X_outlier$num_records)) < 3
-b_ntc <- abs(scale(train_X_outlier$num_total_credit)) < 3
-b_rb <- abs(scale(train_X_outlier$revol_balance)) < 3 #
-b_ru <- abs(scale(train_X_outlier$revol_util)) < 3 #only three outliers, we can remove them
+train_X_outlier = train_X_impute
+
+#define outlier function that replaces the outlier with a mean value?
+handle_outlier_z <- function(col){
+  col_z <- scale(col)
+  ifelse(abs(col_z)>3,
+         sign(col_z)*3*attr(col_z,"scaled:scale") + attr(col_z,"scaled:center"), col)
+}
+
+#We now change all outlier values to NA values
+num.cols <- sapply(train_X, is.numeric)
+train_X_outlier[,all_cols[2:13]] <-  sapply(train_X[,all_cols[2:13]], FUN = handle_outlier_z)
+
+#we see several of the columns now containg NA values
+for (i in all_cols[2:13]){
+  print(paste0("column name:",i,"  ",sum(abs(scale(train_X_outlier[i]))>3)))
+}
+
+#We impute those NA values again like step 1
+impute <- function(x, method = median, val = NULL) {
+  if (is.null(val)) {
+    val <- method(x, na.rm = TRUE)
+  }
+  x[is.na(x)] <- val
+  return(x)
+}
+
+num.cols <- sapply(train_X_impute, is.numeric) 
+train_X_outlier[, num.cols] <- lapply(train_X_outlier[, num.cols],
+                                     FUN = impute,
+                                     method = median)
+
+write.csv(train_X_outlier, file= "../data/silver/train_cleaned_NA_data.csv")
+
+#we no longer have any NA values and all original outliers were replaced by the mean value
+
+############################################
+#3.3 Specific outliers with 'expert' opinion
+############################################
+
+train_X_outlier = train_X_impute
+
+b_ai2 <- abs(scale(train_X_outlier$annual_income)) < 3 #INCREDIBLY skewed results, remove them all
+b_dti2 <- (train_X_outlier$debt_to_income < 1500) #Only one value that is bad (1600+), 
+#remove this value, also self reported so could be wrong data, remove!
+b_ir2 <- NULL #outliers are not really that bad 
+#and the values make sens, so we don't remove any of them (see boxplots)
+b_mp2 <- NULL # gradual increase of our outlier, 922 outliers but there are 
+#no crazy values so we keep them
+b_nb2 <- NULL #Very important! Don't drop these! 
+#this shows us that the people that take out a loan are people with 0 or 7 bankruptcies! 
+b_nm2 <- abs(scale(train_X_outlier$num_mortgages)) < 3 #the values are up to three times more
+#than the limit of our boxplot, we do remove these values
+b_oc2 <- abs(scale(train_X_outlier$num_open_credit)) < 3 #almost all of the observations are 
+#contained up until thirty so we remove the outliers
+b_nr2 <- abs(scale(train_X_outlier$num_records)) < 3 #99% is contained in the first three
+#categories of number of records so we remove the outliers
+b_ntc2 <- abs(scale(train_X_outlier$num_total_credit)) < 3 #majority of the observations are 
+#normally distributed so we remove the outliers
+b_rb2 <- NULL #outliers are very close to the boxplots so
+#keep them, also no steep increase but a gradual increase in outliers
+b_ru2 <- abs(scale(train_X_outlier$revol_util)) < 3 #only three outliers, we can remove them
+
+new_train_x2 <- subset(train_X_outlier, (b_ai2&b_dti2&b_nm2&b_oc2&b_nr2&b_ntc2&b_ru2))
+
+#We have 66407 rows se we removed 4547 rows
+
+write.csv(new_train_x2, file= "../data/silver/train_cleaned_less_restrictive_data.csv")
 
